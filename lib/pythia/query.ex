39246d5b58493c184e5770ad5f5e51b082059d08
srp2930 <s.rimingtonpounder@gmail.com>
2016-08-01 15:14:45 +0100

    refactored search parameters

--- lib/pythia/query.ex
+++ lib/pythia/query.ex
@@ -3,17 +3,6 @@ defmodule Pythia.Query do
   alias Pythia.Data
   use Ecto.Schema
 
-  defp database_search(keyword) do
-    Pythia.Repo.all(from d in Data, select: d)
-    |> IO.inspect
-    |> Enum.map(fn x -> if (includes_query(x.title, keyword) || includes_query(x.description, keyword) || includes_query(x.url, keyword)) do x end end)
-    |> Enum.drop_while(fn x -> x == nil end)
-  end
-
-  defp includes_query(data, query) do
-    if data != nil, do: String.contains?(String.downcase(data), query)
-  end
-
   def split_string(query) do
   String.split(query, " ")
   |> Enum.map(fn x -> database_search(x) end)
@@ -21,6 +10,21 @@ defmodule Pythia.Query do
   |> Enum.uniq
   end
 
+  defp database_search(keyword) do
+    Pythia.Repo.all(from d in Data, select: d)
+    |> Enum.map(fn x -> if (search_parameters(x, keyword)) do x end end)
+    |> Enum.drop_while(fn x -> x == nil end)
+  end
 
+  defp includes_query(data, query) do
+    if data != nil, do: String.contains?(String.downcase(data), query)
+  end
 
+  defp search_parameters(data, keyword) do
+    [data.title, data.description, data.url]
+    |> Enum.map(&includes_query(&1, keyword))
+    |> IO.inspect
+    |> Enum.any?(&(&1 == true))
+    # (includes_query(data.title, keyword) || includes_query(data.description, keyword) || includes_query(data.url, keyword))
+  end
 end
